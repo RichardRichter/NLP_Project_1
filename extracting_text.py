4139aa87022bd019ad8b2e0fd33c37ea89a2cb58
RichardRichter <36464441+RichardRichter@users.noreply.github.com>
2021-10-12 16:34:06 -0500

    added comments

--- extracting_text.py
+++ extracting_text.py
@@ -1,7 +1,7 @@
-import json
-f = open('gg2013.json')
-data = json.load(f)
+import json #needs to be imported to read json files
+f = open('gg2013.json') 
+data = json.load(f) #deserializes the json file to a python object
 len(data)
-my_data = []
+my_data = [] # creates a list for our tweets
 for i in data:
-  my_data.append(i['text'])
+  my_data.append(i['text']) # for every tweet in the data, its going to extract the text section
