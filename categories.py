842d6866e1617b00941038372aae5d8d03fd8610
Nathan Heidacker <nathanheidacker@gmail.com>
2021-10-13 15:14:34 -0500

    categories progress

--- categories.py
+++ categories.py
@@ -1,15 +1,16 @@
 import spacy
 import re
+import string
 from collections import Counter
 from extraction import load_tweets
 
 # nlp is the 'natural language processor'
 nlp = spacy.load('en_core_web_sm')
 data = load_tweets()
-potential_categoroies = []
+potential_categories = []
 
 # Common phrases that could precede or succeed category names
-patterns = [r'.*\bnominated for\b(.*)', r'.*\bwon\b(.*)', r'.*\bawarded\b(.*)', r'.*\baward for\b(.*)', r'.*\bawarded the\b(.*)', r'(.*)\bawarded\b(.*)', r'.*\bwinner of\b(.*)']
+patterns = [r'.*\bnominated for (best\b.*)', r'.*\bwon (best\b.*)', r'.*\bawarded (best\b.*)', r'.*\baward for (best\b.*)', r'.*\bawarded the (best\b.*)', r'(.*)\bawarded (best\b.*)', r'.*\bwinner of (best\b.*)']
 
 # Collecting tweets with matches
 for tweet in data:
@@ -18,9 +19,12 @@ for tweet in data:
 		if match:
 
 			# We only want the text that corresponds to where the award would be in the tweet (use group 1)
-			potential_categoroies.append(match.group(1))
+			potential_categories.append(match.group(1))
 			break
 
-#
-print(len(potential_categoroies))
-print(potential_categoroies[0:10])
+# Cleaning strings (all alpha)
+potential_categories = [re.sub(r'[^a-z ]+', '', text) for text in potential_categories]
+
+
+print(len(potential_categories))
+print(potential_categories[0:-1])
