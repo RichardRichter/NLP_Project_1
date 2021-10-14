0a25ad5471a23be83b0b6279cc799775c6661b09
Nathan Heidacker <nathanheidacker@gmail.com>
2021-10-14 12:50:06 -0500

    refactoring / cleaning up

--- categories.py
+++ categories.py
@@ -4,13 +4,93 @@ import string
 from collections import Counter
 from extraction import load_tweets
 
-# nlp is the 'natural language processor'
+# nlp engine
 nlp = spacy.load('en_core_web_sm')
-data = load_tweets()
-processed = []
 
-# Common phrases that could precede or succeed category names
-patterns = [r'.*\bnominated for (best\b.*)', r'.*\bw[io]ns? (best\b.*)', r'.*\bawarded (best\b.*)', r'.*\baward for (best\b.*)', r'.*\bawarded the (best\b.*)', r'.*\bwinner of (best\b.*)', r'.*\b(best\b.*)goes to\b.*', r'.*\b(best\b.*)awarded to\b.*', r'.*\b(best\b.*)won by\b.*']
+# Takes in a list of strings, returns a list of strings containing only alpha characters [a-z]
+def make_alpha(tweets):
+	return [re.sub(r'[^a-z ]+', '', text.lower()) for text in tweets]
+
+# Reduce the number of words in each tweet (the beginning words should contain the award name)
+def shorten(tweets, limit=10):
+	result = []
+	for tweet in tweets:
+		words = tweet.split(' ')
+		short = ' '.join(words[0:limit])
+		result.append(short)
+
+	return result
+
+# Returns a list of tweets that matches any of the given patterns
+def match_any(tweets, patterns):
+	result = []
+	for tweet in data:
+		for pattern in patterns:
+			match = re.match(pattern, tweet)
+			if match:
+
+				# We only want the text that corresponds to where the award would be in the tweet (use group 1)
+				result.append(match.group(1))
+				break
+
+	return result
+
+# Given a list of tweets, returns a Counter of all the words
+def count_words(tweets):
+	word_count = Counter()
+	for tweet in tweets:
+		word_count += Counter(tweet.split(' '))
+
+	return word_count
+
+# Given a list of words, filters a set of tweets to only include those words
+def filter_words(filter_words, tweets):
+	result = []
+	for tweet in tweets:
+		words = tweet.split(' ')
+		filtered = ' '.join([word for word in words if word in filter_words])
+		result.append(filtered)
+
+	return result
+
+
+
+# Grabbing common phrases that could precede or succeed category names
+# These are patterns that guarantee that award names will precede them
+patterns_specific = [r'.*\bnominated for (best\b.*)', r'.*\bw[io]ns? (best\b.*)', r'.*\bawarded (best\b.*)', r'.*\baward for (best\b.*)', r'.*\bawarded the (best\b.*)', r'.*\bwinner of (best\b.*)', r'.*\b(best\b.*)goes to\b.*', r'.*\b(best\b.*)awarded to\b.*', r'.*\b(best\b.*)won by\b.*']
+
+# This pattern is much more broad, will grab everything
+patterns = [r'.*\b(best\b.*)']
+
+# Loadint in the tweets from the json
+data = load_tweets()
+
+# Cleaning the tweets by making them alpha and lowercase
+data = make_alpha(data)
+
+# Shortening the tweets to 10 words, which should cover all of the award names
+data = shorten(data)
+
+# Gathering all tweets that match a certain set of regex patterns
+data = match_any(data, patterns_specific)
+
+# Gathering a count of all words in the processed tweets
+common = count_words(data)
+
+# Returning the 100 most common words as a list of strings
+common = [word[0] for word in common.most_common(200)]
+
+# Filtering tweets to contain only the most common words
+data = filter_words(common, data)
+
+# Results of all this processing
+categories = Counter(data)
+for category in categories.most_common(20):
+	print(category)
+
+
+
+"""
 
 # Collecting tweets with matches
 for tweet in data:
@@ -41,7 +121,7 @@ for tweet in processed:
 		common[word] += 1
 
 # Grabbing the 50 most common words
-common = [word[0] for word in common.most_common(50)]
+common = [word[0] for word in common.most_common(100)]
 
 # Filtering out uncommon/rare words from the processed category tweets
 filtered = []
@@ -55,8 +135,12 @@ for tweet in processed:
 
 processed = filtered[:]
 count_categories = Counter(processed)
-print(count_categories.most_common(10))
+#print(count_categories.most_common(20))
+for item in count_categories.most_common(50):
+	print(item)
 
 
 print(len(processed))
 #print(processed[0:-1])
+
+"""
