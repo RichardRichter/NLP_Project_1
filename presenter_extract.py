e9a451b50a818e0ee5aec2039fbf16805b1a4399
RichardRichter <36464441+RichardRichter@users.noreply.github.com>
2021-10-23 20:17:18 -0500

    Update presenter_extract.py

--- presenter_extract.py
+++ presenter_extract.py
@@ -1,6 +1,15 @@
 import spacy
+import re
 import spacy import displacy
 from extraction import load_tweets
-from 
 
-nlp = spacy.load
+
+nlp = spacy.load("en_core_web_sm", disable=['tok2vec', 'tagger', 'parser', 'attribute_ruler', 'lemmatizer'])
+
+pattern = re.compile(r'present(t|ted|ts|ing)') 
+
+lines = load_tweets()
+
+for line in lines:
+   
+
