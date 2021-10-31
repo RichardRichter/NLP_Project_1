f898a7bc696c922bbe35db16453f397e6ebb7525
Nathan Heidacker <45582949+nathanheidacker@users.noreply.github.com>
2021-10-31 18:57:07 -0500

    Update extraction.py

--- extraction.py
+++ extraction.py
@@ -1,46 +1,53 @@
 import json
 import pickle
 
+def save_tweets(year, raw_json='gg2013.json'):
 
-def save_tweets(filename='tweets2', raw_json='gg2015.json'):
-    # Grabbing all of the necessary text data
-    data = [tweet['text'] for tweet in json.load(open(raw_json))]
+	filename = str(year) + 'tweets'
 
-    # Outputting the data to a pickle
-    with open(filename, 'wb') as test:
-        pickle.dump(data, test)
+	# Grabbing all of the necessary text data
+	data = [tweet['text'] for tweet in json.load(open(raw_json))]
 
-
-def load_tweets(filename='tweets2', num_tweets=-1):
-    try:
-        with open(filename, 'rb') as tweets:
-
-            # Loading the pickle
-            data = pickle.load(tweets, encoding='utf-8')
-
-            # Returning the number of tweets we want
-            return data[0:(-1 if num_tweets > len(data) else num_tweets)]
-
-    # If we can't find the file under filename, create it
-    except FileNotFoundError:
-        save_tweets(filename)
-        return load_tweets(filename, num_tweets)
+	# Outputting the data to a pickle
+	with open(filename, 'wb') as test:
+		pickle.dump(data, test)
 
 
-def load_answers(year=2013):
-    if year not in [2013, 2015]:
-        return
-    else:
-        filename = str(year) + 'answers'
+def load_tweets(year):
 
-    try:
-        with open(filename, 'rb') as answers:
+	filename = str(year) + 'tweets'
 
-            categories = pickle.load(answers, encoding='utf-8')
-            return categories
+	try:
+		with open(filename, 'rb') as tweets:
 
-    except FileNotFoundError:
-        answers = [category for category in json.load(open('2013answers.json'))['award_data']]
-        with open(filename, 'wb') as x:
-            pickle.dump(answers, x)
-        return load_answers()
+			# Loading the pickle
+			data = pickle.load(tweets, encoding='utf-8')
+	
+			# Returning the number of tweets we want
+			return data
+
+	# If we can't find the file under filename, create it
+	except FileNotFoundError:
+		save_tweets(filename)
+		return load_tweets(filename)
+
+
+def load_answers(year):
+
+	if year not in [2013, 2015]:
+		return
+
+	else:
+		filename = str(year) + 'answers'
+
+	try:
+		with open(filename, 'rb') as answers:
+
+			categories = pickle.load(answers, encoding='utf-8')
+			return categories
+
+	except FileNotFoundError:
+		answers = [category for category in json.load(open('2013answers.json'))['award_data']]
+		with open(filename, 'wb') as x:
+			pickle.dump(answers, x)
+		return load_answers()
